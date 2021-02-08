80b819d18cd7b07dfe8c5c89668918c122b0817c
musashi-kitaura <dmchyd42@gmail.com>
2021-02-08 20:16:51 +0900

    修正2

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # get 'categorys/index'
   devise_for :users
   root 'tops#index'
-  resources :users ,only: [:index, :show] do
+  resources :users ,only: [:index, :show,:edit,:update] do
     member do
       get 'favorite'
     end
