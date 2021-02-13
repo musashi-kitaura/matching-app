b664a4bf410b671452a9cc15e127e0e651eafd93
musashi-kitaura <dmchyd42@gmail.com>
2021-02-13 16:03:59 +0900

    修正

--- app/javascript/image_new.js
+++ app/javascript/image_new.js
@@ -1,39 +1,15 @@
-// $(function () {
-//   $fileField = $('#upload-icon')
-//   // fileFieldにidを代入
-//   $($fileField).on('change', $fileField, function (e) {
-//     var file = e.target.files[0]
-//     var reader = new FileReader(),
-//       // new FileReader()これでファイルを読み込む
-//       var preview = $("profile-main__image--icon__input__image2");
-//       // 
-//     reader.onload = (function (file) {
-//       //読み込みが成功した場合(onloadイベント内)
-//       return function (e) {
-//         $preview.empty();
-//         $preview.append($('<img>').attr({
-//           src: e.target.result,
-//           width: "100%",
-//           class: "preview",
-//           title: file.name
-//         }));
-//       };
-//     })(file);
-//     reader.readAsDataURL(file);
-
-//     //画像ファイルの場所をURLとして読み込む
-//   });
-// });
 $(function(){
   $('#upload-icon').on('change', function (e) {
     const file = e.target.files[0]
+    console.log(file)
+    //target.files[0]をつくる
     const reader = new FileReader();
-    const preview = $("profile-main__image--icon__input__image2");
     reader.onload = (function (file) {
     //読み込みが成功した場合(onloadイベント内)
     return function (e) {
-      $(preview).empty("img");
-      preview.append($('img').attr({
+      $(".profile-main__image--icon__input__image").remove();
+      $(".profile-main__image--icon__input__image2").empty();
+      $(".profile-main__image--icon__input__image2").append($('<img>').attr({
       src: e.target.result,
       width: "100%",
       class: "preview",
@@ -42,7 +18,5 @@ $(function(){
     };
   })(file);
   reader.readAsDataURL(file);
-
-  
   });
 });
