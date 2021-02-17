b20f0e1efbae0b8a68133ca76767d733778a1bae
musashi-kitaura <dmchyd42@gmail.com>
2021-02-18 00:11:39 +0900

    修正

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,5 +1,6 @@
 class Community < ApplicationRecord
   has_many :community_users
+  has_many :users,through: :community_users
   belongs_to :category,optional: true
   mount_uploader :image, ImageUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,11 @@ Rails.application.routes.draw do
       get 'favorite'
     end
   end  
-  resources :community_users, only: [:create] 
   resources :communities, only: [:index, :show, :create] 
   resources :relationships, only: [:index,:create, :destroy,]
   resources :categories, only: [:show] 
   resources :rooms, only: [:index, :create, :show]  
-  resources :messages, only: [:index,:create] 
+  resources :messages, only: [:index,:create]
+  resources :community_users, only: [:create] 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
