92b9f17e40840565f7fbd891d53dcf9168cee3bf
musashi-kitaura <dmchyd42@gmail.com>
2021-02-20 18:45:38 +0900

    修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,15 @@
 Community.create!(
   [
     {name: 'モンハン',image:File.open("app/assets//images/20200515-00178549-roupeiro-000-9-view.jpg"),category_id: 1},
+    {name: 'モンハン',image:File.open("app/assets//images/20200515-00178549-roupeiro-000-9-view.jpg"),category_id: 1},
+    {name: 'モンハン',image:File.open("app/assets//images/20200515-00178549-roupeiro-000-9-view.jpg"),category_id: 1},
+    {name: 'モンハン',image:File.open("app/assets//images/20200515-00178549-roupeiro-000-9-view.jpg"),category_id: 1},
+    {name: 'モンハン',image:File.open("app/assets//images/20200515-00178549-roupeiro-000-9-view.jpg"),category_id: 1},
+    {name: 'モンハン',image:File.open("app/assets//images/20200515-00178549-roupeiro-000-9-view.jpg"),category_id: 1},
+    {name: 'モンハン',image:File.open("app/assets//images/20200515-00178549-roupeiro-000-9-view.jpg"),category_id: 1},
+    {name: 'モンハン',image:File.open("app/assets//images/20200515-00178549-roupeiro-000-9-view.jpg"),category_id: 1},
+    {name: 'モンハン',image:File.open("app/assets//images/20200515-00178549-roupeiro-000-9-view.jpg"),category_id: 1},
+    {name: 'モンハン',image:File.open("app/assets//images/20200515-00178549-roupeiro-000-9-view.jpg"),category_id: 1},
     {name: 'ウイニングイレブン',image:File.open("app/assets/images/d50711-181-204570-12.jpg"),category_id: 2},
   ]
 )   
@@ -16,5 +25,11 @@ Category.create!(
   [
     {name: 'アクションゲーム',image:File.open("app/assets//images/20200515-00178549-roupeiro-000-9-view.jpg")},
     {name: 'シューティングゲーム',image:File.open("app/assets//images/20200515-00178549-roupeiro-000-9-view.jpg")},
+    {name: 'シュミレーションゲーム',image:File.open("app/assets//images/20200515-00178549-roupeiro-000-9-view.jpg")},
+    {name: 'レーシングゲーム',image:File.open("app/assets//images/20200515-00178549-roupeiro-000-9-view.jpg")},
+    {name: 'アドベンチャーゲーム',image:File.open("app/assets//images/20200515-00178549-roupeiro-000-9-view.jpg")},
+    {name: 'ロールプレイングゲーム',image:File.open("app/assets//images/20200515-00178549-roupeiro-000-9-view.jpg")},
+    {name: 'パズルゲーム',image:File.open("app/assets//images/20200515-00178549-roupeiro-000-9-view.jpg")},
+    {name: '音楽ゲーム',image:File.open("app/assets//images/20200515-00178549-roupeiro-000-9-view.jpg")},
   ]
 )  
